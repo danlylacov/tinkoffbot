0d78ca36d2c1e5d87b684b4ff74c6a306df416f4
danlylacov <99447254+danlylacov@users.noreply.github.com>
2023-08-30 00:37:31 +0300

    Update standrart_deviation.py

--- volume_analyze/Standart deviation and Z-scope/standrart_deviation.py
+++ volume_analyze/Standart deviation and Z-scope/standrart_deviation.py
@@ -1,5 +1,6 @@
 import sqlite3
-import asyncio
+from stream_parser.parser import stream_parse
+from math import sqrt
 
 class StandartDeviation(object):
 
@@ -9,7 +10,7 @@ class StandartDeviation(object):
         self.cur = self.historical_data_bd.cursor()
 
 
-    def get_avarage_of_action_volume(self, figi): # str -> float
+    def get_avarage_of_action_volume(self, figi: str): # str -> float
         '''
         Функция получения среденего значения объёма после отсечения 25% самых мелких и 25% самых крупных значений по акции
         :param figi: figi-индефикатор акции
@@ -24,15 +25,51 @@ class StandartDeviation(object):
         return sum(result)/len(result)
 
 
-    def get_sorted_volumes(self): # -> list
+    def get_sorted_volumes(self, figis: list): # list -> list
         '''
-        Функция получения списка средних значений объема по акциям передаваемым, как аргумент класса
+        Функция получения списка средних значений объема по акциям указанным как список
         :return: список, имеющий структуру: [ [figi акции, среднее зн-е], [..], [..] ]
         '''
         result = []
-        for figi in self.figis:
+        for figi in figis:
             result.append([figi, self.get_avarage_of_action_volume(figi)])
         return result
+
+
+    def get_value_deviation(self, volumes: list, avarage_value: float):# list, float -> list
+        '''
+        Функция получения разницы (отклонения) между значением каждого объема и средним значением объема
+        :param volumes: зн-я объёмов
+        :param avarage_value: среднее зн-е объема
+        :return: список с разницами
+        '''
+        deviations = [volume - avarage_value for volume in volumes]
+        return deviations
+
+    def get_standart_deviation(self, deviations: list): # list -> float
+        '''
+        Функция получения среднеквадратичного отклонения
+        :param deviations: список с отклонениями
+        :return: среднеквадратичное отклонение
+        '''
+        deviations = [deviation ** 2 for deviation in deviations]
+        standart_deviation = sqrt(sum(deviations)/len(deviations))
+        return standart_deviation
+
+
+    def get_Z_analyize(self, X_i: float, X_av: float, standard_deviation: float): # float, float, float -> float
+        '''
+        Функция получения Z-преобразования по формуле Z_i =  Z_i = (X_i - X_av)/deviation
+        :param X_i: значение объема, которое мы получаем в реальном времени
+        :param X_av: среднее значение
+        :param standart_deviation: среднеквадратическое отклонение
+        :return: проценты (то есть величина, показывающая, в скольких стандратных отклонениях от среднего значения находится изучаемое значение объема).
+        '''
+        Z_i = (X_i - X_av)/standard_deviation
+        return Z_i
+
+
+
 
 
 
@@ -41,7 +78,10 @@ class StandartDeviation(object):
 
 
 a = StandartDeviation(['BBG000BX7DH0', 'BBG000DBD6F6'])
-print(a.get_sorted_volumes())
+print(a.get_value_deviation([1, 2, 3], 1))
+
+
+
 
 
 
